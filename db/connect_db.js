8c3c21d3c7963dabff69118f2978ade688ee4809
Abhaydixi <dixitabhay633@gmail.com>
2023-05-27 00:10:57 +0530

    live url

--- db/connect_db.js
+++ db/connect_db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 const url = "mongodb://127.0.0.1:27017/addimission"
+const live_URL = 'mongodb+srv://dixitabhay633:rudra1234@cluster1.d8nmtdf.mongodb.net/addmissionportel?retryWrites=true&w=majority'
 
 const connectDB = () => {
     // For local DB
-    return mongoose.connect(url)
+    return mongoose.connect(live_URL)
 
 
         // For cloud DB
